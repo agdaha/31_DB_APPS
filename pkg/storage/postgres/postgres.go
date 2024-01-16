8b7ce37255ebf4f5f57d47f288529639f1f856f9
Rishat Tarisov <r.tarisov@gmail.com>
2024-01-16 09:23:39 +0500

    Update pkg/storage/postgres/postgres.go
    Co-authored-by: rs2410 <47646081+rs2410@users.noreply.github.com>

--- pkg/storage/postgres/postgres.go
+++ pkg/storage/postgres/postgres.go
@@ -14,7 +14,7 @@ type Storage struct {
 func New(constr string) (*Storage, error) {
 	db, err := pgxpool.Connect(context.Background(), constr)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("connecting to db: %v", err)
 	}
 	s := Storage{
 		db: db,
